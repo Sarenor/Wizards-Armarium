6d4eedc600333c189e28774453a4c583f9bfc768
Dominik Röschke <dominik.roeschke@web.de>
2022-02-15 14:34:35 +0100

    Wizards Armarium now also switches between Spellfoci

--- src/main/java/de/sarenor/arsinstrumentum/items/curios/armarium/ArmariumSlot.java
+++ src/main/java/de/sarenor/arsinstrumentum/items/curios/armarium/ArmariumSlot.java
@@ -19,14 +19,17 @@ import static de.sarenor.arsinstrumentum.utils.SerializationUtiils.serializeItem
 public class ArmariumSlot {
     private static final String ARMARIUM_ARMOR_TAG = ArsInstrumentum.MODID + "_armarium_armor_tag";
     private static final String ARMARIUM_HOTBAR_TAG = ArsInstrumentum.MODID + "_armarium_hotbar_tag";
+    private static final String ARMARIUM_SPELLFOCUS_TAG = ArsInstrumentum.MODID + "_armarium_spellfocus_tag";
 
     private List<ItemStack> armor = new ArrayList<>();
     private List<ItemStack> hotbar = new ArrayList<>();
+    private List<ItemStack> spellfoci = new ArrayList<>();
 
     public static ArmariumSlot deserialize(CompoundTag compoundTag) {
         ArmariumSlot armariumSlot = new ArmariumSlot();
         armariumSlot.setArmor(deserializeItemList(compoundTag, ARMARIUM_ARMOR_TAG));
         armariumSlot.setHotbar(deserializeItemList(compoundTag, ARMARIUM_HOTBAR_TAG));
+        armariumSlot.setSpellfoci(deserializeItemList(compoundTag, ARMARIUM_SPELLFOCUS_TAG));
         return armariumSlot;
     }
 
@@ -34,6 +37,7 @@ public class ArmariumSlot {
         CompoundTag serialized = new CompoundTag();
         serialized.put(ARMARIUM_ARMOR_TAG, serializeItemList(armor));
         serialized.put(ARMARIUM_HOTBAR_TAG, serializeItemList(hotbar));
+        serialized.put(ARMARIUM_SPELLFOCUS_TAG, serializeItemList(spellfoci));
         return serialized;
     }
 
